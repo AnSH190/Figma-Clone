58e36e6a3fa07273e6645279965699101fd4004b
Ansh Vaswani <anshvaswani73@gmail.com>
2024-02-05 14:39:22 +0530

    Real time cursor and chat feature

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,13 @@
+'use client'
+
 import { Room } from "./Room";
 import { CollaborativeApp } from "./CollaborativeApp";
+import Live from "@/components/Live";
 
 export default function Page() {
   return (
-    <div className="h-[100vh] text-center w-full flex justify-center items-center">
-      <h1 className="text-2xl text-white">Liveblocks figma Clone HomePage </h1>
+    <div>
+      <Live />
     </div>
   );
 }
